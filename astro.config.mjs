0beb88eaa4b25d006196038195907077006040f0
sakshyam-sh <sakshyamshrestha27@gmail.com>
2026-01-07 15:59:17 +0545

    fix: init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+    site: "https://example.com",
+    integrations: [mdx(), sitemap(), icon()],
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true,
+        },
+    }),
 });
